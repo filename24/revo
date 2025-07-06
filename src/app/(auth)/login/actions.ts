a81ed0acbe5a546cf62b0444eb75fd374a06242a
__filename <filename@inft.kr>
2025-07-06 22:57:02 +0800

    fix(auth): changed client actions

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -1,4 +1,4 @@
-'use server'
+'use client'
 
 import { signIn } from '@/lib/auth'
 
--- src/app/(auth)/signup/actions.ts
+++ src/app/(auth)/signup/actions.ts
@@ -1,4 +1,4 @@
-'use server'
+'use client'
 
 import { signUp } from '@/lib/auth'
 import { SignupSchema } from '@/schemas/auth'
